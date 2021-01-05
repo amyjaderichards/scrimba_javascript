768e6cfd70e4f35976353a0d1a315153b14117ee
Amy <amyjaderichards@googlemail.com>
2021-01-05 20:59:56 +0000

    Exercise 9 completed

---
+++ 09-Arrow_Functions/index.js
@@ -0,0 +1,11 @@
+const squareNum = function(num) {
+    return num * num;
+}
+
+const squareNumArrow = (num) => {
+    return num * num;
+}
+
+const squareNum = num => num * num;
+
+console.log(squareNum(12));
